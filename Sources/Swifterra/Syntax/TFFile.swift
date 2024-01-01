af0091778f362b693d526e57b8159f70e550a4ad
omochimetaru <omochi.metaru@gmail.com>
2024-01-01 11:44:36 +0900

    remove file api

--- Sources/Swifterra/Syntax/TFFile.swift
+++ Sources/Swifterra/Syntax/TFFile.swift
@@ -21,9 +21,4 @@ public struct TFFile {
         let r = Renderer()
         return try r.render(file: self)
     }
-
-    public func writeIfChange(to file: URL) throws {
-        let data = try render().data(using: .utf8)!
-        try Files.write(data: data, to: file)
-    }
 }
